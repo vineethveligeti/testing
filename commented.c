ad17435b92a305ff220e456324bb485633ce5751
vv88 <vvveligeti@gmail.com>
2020-05-21 17:05:39 +0530

    uploaded a file

---
+++ commented.c
@@ -0,0 +1,8 @@
+// #include <stdio.h>
+// int main(){
+//     int a=1, b[3] = {1,2,3};
+//     printf("%d\n",a);
+//     for(int i=0;i<3;i++) printf("%d ",b[i]);
+//     printf("\n");
+//     return 0;
+// }
